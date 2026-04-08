d4120461be65cbb6430c6cdbebf7af2e4109637f
damilola <damilolabalogun0407@gmail.com>
2026-04-08 13:37:01 +0100

    Updated devops app

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "DevOps Project Deployed on Render, Hello! Dockerized app"
+    return "DevOps Project Deployed on Render, Hello! Dockerized app" 
+    return "Hello! Devops app running"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
